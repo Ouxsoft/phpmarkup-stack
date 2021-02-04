5147f12bb5010b555c282abfee71217905560acd
hxtree <matthewheroux@gmail.com>
2021-02-03 20:20:36 -0600

    Add sphinx docs and environmental specific docker installs

---
+++ docker/php-fpm/install.sh
@@ -0,0 +1,25 @@
+# Dockerfile installation script for running environmental specific commands
+
+# create php.ini
+if [ "$ENV" = "PROD" ]; then
+  mv $PHP_INI_DIR/php.ini-production $PHP_INI_DIR/php.ini
+else
+  mv $PHP_INI_DIR/php.ini-development $PHP_INI_DIR/php.ini
+fi
+
+# install xdebug on dev only
+if [ "$ENV" = "DEV" ]; then
+  pecl install xdebug
+  echo "xdebug.remote_enable=on\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  echo "xdebug.remote_autostart=on\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  echo "xdebug.remote_port=9000\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  echo "xdebug.remote_handler=dbgp\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  echo "xdebug.remote_connect_back=1\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  docker-php-ext-enable xdebug
+  docker-php-source delete
+  rm -rf /tmp/*
+fi
+
+# install composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
---
+++ docker/sphinx-docs/config/build/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. LivingMarkup documentation master file, created by
+   sphinx-quickstart on Mon Feb  1 03:52:04 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to LivingMarkup's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
