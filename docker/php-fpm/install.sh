4086d22d26d13ecadab41254dc1941b682c3fb61
hxtree <matthewheroux@gmail.com>
2021-02-10 20:57:24 -0600

    Add doxygen

--- docker/php-fpm/install.sh
+++ docker/php-fpm/install.sh
@@ -10,8 +10,8 @@ fi
 # install xdebug on dev only
 if [ "$ENV" = "DEV" ]; then
   pecl install xdebug
-  echo "xdebug.remote_enable=on\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-  echo "xdebug.remote_autostart=on\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  echo "xdebug.mode=debug\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  echo "xdebug.start_with_request=yes\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
   echo "xdebug.remote_port=9000\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
   echo "xdebug.remote_handler=dbgp\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
   echo "xdebug.remote_connect_back=1\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
