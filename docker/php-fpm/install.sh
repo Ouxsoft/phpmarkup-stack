32e0c9db5a23a2131a13b856e842a2971cecab7c
hxtree <matthewheroux@gmail.com>
2021-02-11 20:17:21 -0600

    Add sphinx container and ./stack run command for making docs on the fly

--- docker/php-fpm/install.sh
+++ docker/php-fpm/install.sh
@@ -10,11 +10,11 @@ fi
 # install xdebug on dev only
 if [ "$ENV" = "DEV" ]; then
   pecl install xdebug
-  echo "xdebug.mode=debug\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  #echo "xdebug.mode=debug\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
   echo "xdebug.start_with_request=yes\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-  echo "xdebug.remote_port=9000\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  echo "xdebug.client_port=9000\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
   echo "xdebug.remote_handler=dbgp\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-  echo "xdebug.remote_connect_back=1\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+  echo "xdebug.discover_client_host=true\n" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
   docker-php-ext-enable xdebug
   docker-php-source delete
   rm -rf /tmp/*
